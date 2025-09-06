e15a8e0e875fc89a868d530ec9757a6f533c1bd2
minmin02 <aktr378@gmail.com>
2025-09-06 17:36:09 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/example/guardpay/GuardpayApplication.java
@@ -0,0 +1,11 @@
+package com.example.guardpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardpayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GuardpayApplication.class, args);
+	}
+}
